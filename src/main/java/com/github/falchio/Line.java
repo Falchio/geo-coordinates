80262c8da1cc9a3b6ae7832cc918c427e0f13a19
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2024-05-14 20:10:57 +0300

    add new method

--- src/main/java/com/github/falchio/Line.java
+++ src/main/java/com/github/falchio/Line.java
@@ -18,4 +18,53 @@ public record Line(Location start, Location end) {
 
         return minDistance * 1_000;
     }
+
+    private static final double RADIUS_OF_EARTH_IN_METERS = 6_371_000;
+
+    /**
+     * <a href="https://stackoverflow.com/a/35587935/12980819">source</a>
+     */
+    public double calculateMinDistanceInMeters(Location midPoint) {
+
+        double lat1 = Math.toRadians(start.lat());
+        double lon1 = Math.toRadians(start.lon());
+        double lat2 = Math.toRadians(end.lat());
+        double lon2 = Math.toRadians(end.lon());
+        double lat3 = Math.toRadians(midPoint.lat());
+        double lon3 = Math.toRadians(midPoint.lon());
+
+
+        double bear12 = bearing(lat1, lon1, lat2, lon2);
+        double bear13 = bearing(lat1, lon1, lat3, lon3);
+        double dis13 = distance(lat1, lon1, lat3, lon3);
+
+        double diff = Math.abs(bear13 - bear12);
+        if (diff > Math.PI) {
+            diff = 2 * Math.PI - diff;
+        }
+
+        double dxa;
+        if (diff > (Math.PI / 2.0)) {
+            dxa = dis13;
+        } else {
+            double dxt = Math.asin(Math.sin(dis13 / RADIUS_OF_EARTH_IN_METERS) * Math.sin(bear13 - bear12)) * RADIUS_OF_EARTH_IN_METERS;
+            double dis12 = distance(lat1, lon1, lat2, lon2);
+            double dis14 = Math.acos(Math.cos(dis13 / RADIUS_OF_EARTH_IN_METERS) / Math.cos(dxt / RADIUS_OF_EARTH_IN_METERS)) * RADIUS_OF_EARTH_IN_METERS;
+            if (dis14 > dis12) {
+                dxa = distance(lat2, lon2, lat3, lon3);
+            } else {
+                dxa = Math.abs(dxt);
+            }
+        }
+
+        return dxa;
+    }
+
+    public static double distance(double latA, double lonA, double latB, double lonB) {
+        return Math.acos(Math.sin(latA) * Math.sin(latB) + Math.cos(latA) * Math.cos(latB) * Math.cos(lonB - lonA)) * RADIUS_OF_EARTH_IN_METERS;
+    }
+
+    public static double bearing(double latA, double lonA, double latB, double lonB) {
+        return Math.atan2(Math.sin(lonB - lonA) * Math.cos(latB), Math.cos(latA) * Math.sin(latB) - Math.sin(latA) * Math.cos(latB) * Math.cos(lonB - lonA));
+    }
 }
--- src/test/java/com/github/falchio/MinimalDistanceFromLocationToLineTest.java
+++ src/test/java/com/github/falchio/MinimalDistanceFromLocationToLineTest.java
@@ -13,14 +13,15 @@ public class MinimalDistanceFromLocationToLineTest {
     @ParameterizedTest
     @MethodSource("provideArguments")
     void calculateMinimalDistance(Line line, Location midPoint, double expected) {
-        assertEquals(expected, line.minDistanceInMeters(midPoint));
+        assertEquals(expected, line.calculateMinDistanceInMeters(midPoint));
     }
 
     private static Stream<Arguments> provideArguments() {
         return Stream.of(
-                Arguments.of(new Line(new Location(-10.1, -55.5), new Location(-15.2, -45.1)), new Location(-10.5, -62.5), 767_100),
-                Arguments.of(new Line(new Location(40.5, 60.5), new Location(50.5, 80.5)), new Location(51, 69), 479_610),
-                Arguments.of(new Line(new Location(21.72, 35.61), new Location(23.65, 40.7)), new Location(25, 42), 19_971_000)
+                Arguments.of(new Line(new Location(-10.1, -55.5), new Location(-15.2, -45.1)), new Location(-10.5, -62.5), 767094.7682446191),
+                Arguments.of(new Line(new Location(40.5, 60.5), new Location(50.5, 80.5)), new Location(51, 69), 479609.29936769704),
+                Arguments.of(new Line(new Location(21.72, 35.61), new Location(23.65, 40.7)), new Location(25, 42), 199706.83877512268),
+                Arguments.of(new Line(new Location(3.227511, 101.724318), new Location(3.222895, 101.719751)), new Location(3.224972, 101.722932), 88.9378439402154)
         );
     }
 
